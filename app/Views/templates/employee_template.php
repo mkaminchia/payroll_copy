771e1042195cb0cc3b3897f4d6ac787089772d29
michaelwahome <michaelwahome1@gmail.com>
2022-07-13 13:40:47 +0300

    Made employee dashboard responsive

--- app/Views/templates/employee_template.php
+++ app/Views/templates/employee_template.php
@@ -19,103 +19,87 @@
     <link href="../css/employee.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <!-- <div class="container-fluid"> -->
-        <header class="mb-5">
-            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/employee">Home</a>
-                    <a class="navbar-brand" href="#">Payslip</a>
-                    <a class="navbar-brand" href="#">Profile</a>
-                    <a class="navbar-brand" href="/employee/logout">Logout</a>
-                </div>
-            </nav>
+    <div class="container-fluid" style="padding: 0;">
+        <header>
+            <div class="topnav" id="myTopnav">
+                <a href="/employee">Home</a>
+                <a href="#">Payslip</a>
+                <a href="#">Profile</a>
+                <a href="#">Logout</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
         </header>
-
 
         <?php $this->renderSection('content'); ?>
 
-
-        <footer class="mb-0 mt-5" style="bottom: 0;" id="footer"> 
-            
-            <div class="footer-top"> 
-                <div class="container"> 
-                    <div class="row"> 
-                        <div class="col-lg-3 col-md-6 footer-links"> 
-                            <h4>Useful Links</h4> 
-                            <ul> 
-                                <li>
-                                    <a href="/">Home</a>
-                                </li> 
-                                <li>
-                                    <a href="/catalogue">Catalogue</a>
-                                </li> 
-                                <li>
-                                    <a href="/auth/login">Login</a>
-                                </li> 
-                                <li>
-                                    <a href="/auth/register">Register</a>
-                                </li> 
-                                <li>
-                                    <a href="#">Back to Top</a>
-                                </li> 
-                            </ul> 
-                        </div> 
-
-                        <div class="col-lg-3 col-md-6 footer-contact"> 
-                            <h4>Contact Us</h4> 
-                            <p>                                 
-                                <strong>Phone:</strong> +254 333 433 333
-                                <br> <strong>Email:</strong> ling@fakedomain.com
-                                <br> 
-                            </p> 
-                        </div> 
-
-                        <div class="col-lg-3 col-md-6 footer-info"> 
-                            <h3>About LingsCars</h3> 
-                            <p>We are a recently founded website that aims to deliver you the car of your dreams at no extra effort.</p> 
-                            <div class="social-links mt-3"> 
-                                <a href="#" class="twitter">
-                                </a> 
-                                <a href="#" class="facebook">
-                                </a> 
-                                <a href="#" class="instagram">
-                                </a> 
-                                <a href="#" class="linkedin">
-                                </a> 
-                            </div> 
-                        </div> 
-                    </div> 
+        <footer>
+            <div class="row footer-top">
+                <div class="col-12 col-md-6 footer-links"> 
+                    <h4>Useful Links</h4> 
+                    <ul> 
+                        <li>
+                            <a href="/employee">Home</a>
+                        </li> 
+                        <li>
+                            <a href="#">Payslip</a>
+                        </li> 
+                        <li>
+                            <a href="#">Profile</a>
+                        </li> 
+                        <li>
+                            <a href="#">Logout</a>
+                        </li> 
+                        <li>
+                            <a href="#">Back to Top</a>
+                        </li> 
+                    </ul> 
                 </div> 
-            </div> 
-            <div class="container"> 
-                <div class="copyright"> &copy; Copyright <strong><span>LingsCars</span></strong>. All Rights Reserved </div> 
-                <div class="credits"> Designed by <a href="#">LingsCars</a> </div> 
+                <div class="col-12 col-md-6 footer-contact"> 
+                    <h4>Need help? Contact an admin</h4> 
+                    <p>                                 
+                        <strong>Phone:</strong> +254 333 433 333
+                        <br> <strong>Email:</strong> adminemail@fakedomain.com
+                        <br> 
+                    </p> 
+                </div>
             </div>
-        </footer>    
+            <div class="row footer-bottom">     
+                <p class="copyright"> &copy; Copyright <strong><span>C. Czarnikow Sugar (EA) Ltd</span></strong> 2022. All Rights Reserved </p> 
+                <p class="credits"> Designed by <a href="#">Michael Development</a> </p>
+            </div> 
+        </footer>
 
 
 
 
+
+
+
+
+
+
+
+    </div>
 
 
     <!--Bootstrap JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
     <script>
-        function search_car() {
-            let input = document.getElementById('searchbar').value
-            input=input.toLowerCase();
-            let x = document.getElementsByClassName('image-and-words');
-            
-            for (i = 0; i < x.length; i++) { 
-                if (!x[i].innerHTML.toLowerCase().includes(input)) {
-                    x[i].style.display="none";
-                }
-                else {
-                    x[i].style.display="";                 
-                }
+
+        /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
             }
-        }
+        } 
+
     </script>
+
 </body>
 </html>
