91f82a0136e1847e6a0f14789e4490cfca227f6c
mkaminchia <michaelkaminchia.mk@gmail.com>
2022-07-14 07:42:22 +0300

    Admin class functions skeleton added

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -5,6 +5,69 @@ namespace App\Controllers;
 
 class Admin extends BaseController
 {
+	//function to view the admin dashboard
+	public function dashboard()
+	{
 
+	}
 
+	//function to view the admin's personal details page
+	public function adminDetails()
+	{
+
+	}
+
+	//function to view the page to edit the admin's personal details
+	public function editAdminDetails()
+	{
+
+	}
+
+	//function to process the edit made to the admin's personal details
+	public function processEditAdmin()
+	{
+
+	}
+
+	//function to view the list of employees
+	public function viewEmployees()
+	{
+
+	}
+
+	//function to delete a selected employee
+	public function deleteEmployee()
+	{
+
+	}
+
+	//function to display the page to register a new employee
+	public function registerEmployee()
+	{
+
+	}
+
+	//function to process the registration of a new employee
+	public function processEmployeeRegistration()
+	{
+
+	}
+
+	//function to view a selected employee's personal details
+	public function viewEmployee()
+	{
+
+	}
+
+	//function to view the page to edit an employees personal details
+	public function editEmployee()
+	{
+
+	}
+
+	//function to process the edit of an employee's details
+	public function processEmployeeEdit()
+	{
+
+	}
 }
