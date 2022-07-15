a8d3d79602c2b00106cbf07f01af8d080f47524f
mkaminchia <michaelkaminchia.mk@gmail.com>
2022-07-15 22:19:36 +0300

    Admin class update

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,72 +2,170 @@
 
 namespace App\Controllers;
 
+use App\Models\EmployeeModel;
 
 class Admin extends BaseController
 {
-	//function to view the admin dashboard
+	//function to view the admin dashboard (uses session data [user_details] from Login.php)
 	public function dashboard()
 	{
-
+		//return view('');
 	}
 
-	//function to view the admin's personal details page
+	//function to view the admin's details page (uses session data [user_details] from Login.php)
 	public function adminDetails()
 	{
-
+		//return view('');
 	}
 
-	//function to view the page to edit the admin's personal details
+	//function to view the page to edit the admin's details (uses session data [user_details] from Login.php)
 	public function editAdminDetails()
 	{
-
+		//return view('');
 	}
 
 	//function to process the edit made to the admin's personal details
 	public function processEditAdmin()
 	{
+		//Create model instance
+		$editAdminModel = new EmployeeModel();
 
+		//Retrieve the admin's employee_id
+		$session = session();
+        $userDetails = $session->get('user_details');
+        $employee_id = $userDetails['employee_id'];
+
+		//Retrieve form data from the editAdminDetails() page
+		if($this->request->getMethod() === 'post')
+        {
+        	$email = $this->request->getPost('email');
+            $phone_no = $this->request->getPost('phone_no');
+        }
+
+		//Send to model
+		$confirmation = $editAdminModel->editEmployee($employee_id, $email, $phone_no);
+
+		//If successful, redirect back to editAdminDetails
+
+		//Test
+		echo $confirmation;
+
+		//return redirect()->to('');
 	}
 
 	//function to view the list of employees
 	public function viewEmployees()
 	{
+		//Create model instance
+		$viewEmployeesModel = new EmployeeModel();
 
+		//Call model function
+		$employeeList = $viewEmployeesModel->viewAllEmployees();
+
+		//Create a session to store a list of all employees
+		$session = session();
+        $session->set('employeeList', $employeeList);
+
+		//View the page
+		//return view('');
 	}
 
 	//function to delete a selected employee
 	public function deleteEmployee()
 	{
+		//Create an instance of the model
+		$deleteEmployeeModel = new EmployeeModel();
 
+		//Retrieve selected employee from the viewEmployees() [Post]
+		if(isset($_GET['delete']))
+	    {
+	        $employee_id = $_GET['delete'];
+	    }
+
+		//Call model function
+		$deleteEmployee = $deleteEmployeeModel->deleteEmployee($employee_id);
+
+		//Redirect to viewEmployee
+		//return redirect()->to('');
 	}
 
 	//function to display the page to register a new employee
 	public function registerEmployee()
 	{
-
+		//return view('');
 	}
 
 	//function to process the registration of a new employee
 	public function processEmployeeRegistration()
 	{
+		//Create model instance
+		$registerEmployeeModel = new EmployeeModel();
 
-	}
+		//Retrieve form data from registerEmployee() page [Post]
+		if($this->request->getMethod() === 'post')
+        {
+        	$firstname = $this->request->getPost('firstname');
+        	$surname = $this->request->getPost('surmane');
+        	$email = $this->request->getPost('email');
+            $phone_no = $this->request->getPost('phone_no');
+            $password = $this->request->getPost('password');
+        }
 
-	//function to view a selected employee's personal details
-	public function viewEmployee()
-	{
+		//Call model function
+		$registerEmployee = $registerEmployeeModel->registerEmployee($firstName, $surname, $email, $phone_no, $password);
 
+		//Redirect to viewEmployee
+		//return redirect()->to('');
 	}
 
 	//function to view the page to edit an employees personal details
 	public function editEmployee()
 	{
+		//Retrieve selected employee from the viewEmployees() [Post]
+		if(isset($_GET['edit']))
+	    {
+	        $employee_id = $_GET['edit'];
+	    }
 
+	    //Call model function
+		$employeeDetails = $employeeFocusModel->employeeFocus($employee_id);
+
+		//Create two sessions:
+			//1. To store employee selected - editEmployee
+			//2. To store the details of the selected employee - employeeDetails
+		$session = session();
+        $session->set('editEmployee', $employee_id);
+        $session->set('editEmployeeDetails', $editEmployeeDetails);
+
+        //View Page
+        //return view('');
 	}
 
 	//function to process the edit of an employee's details
 	public function processEmployeeEdit()
 	{
+		//Create an instance of the model
+		$editEmployeeModel = new EmployeeModel();
 
+		//Retrieve the employee_id of the selected employee
+		$session = session();
+        $employee_id = $session->get('editEmployee');
+
+		//Retrieve form data from editEmployee() page [Post]
+		if($this->request->getMethod() === 'post')
+        {
+        	$email = $this->request->getPost('email');
+            $phone_no = $this->request->getPost('phone_no');
+        }
+
+		//Send to model
+		$confirmation = $editEmployeeModel->editEmployee($employee_id, $email, $phone_no);
+
+		//If successful, redirect back to editAdminDetails
+
+		//Test
+		echo $confirmation;
+
+		//return redirect()->to('');
 	}
 }
--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -40,4 +40,117 @@ class EmployeeModel extends Model
 		//Model Test
 		//return "Data Transfered to Model Successfully: Email - ".$email.", Password - ".$password; 
 	}
+
+	public function editEmployee($employee_id, $email, $phone_no)
+	{
+		//Query
+		if ($this->db->query("UPDATE employees SET email = '$email',  phone_no = '$phone_no' WHERE employee_id = '$employee_id'"))
+		{
+		    $confirmation = "Successful";
+		}
+		else
+		{
+		    $confirmation = "Unsuccessful";
+		}
+
+		//Return array
+		return $confirmation;
+	}
+
+	public function viewAllEmployees()
+	{
+		//Temporarily define $user_info
+		$employeeList = array();
+
+		//Query
+		$query = $this->db->query("
+			SELECT employee_ID, firstname, surname, email, phone_no
+			FROM employees 
+			WHERE is_deleted = 0 AND role_id = "1"
+			");
+
+		//Store details in array
+		foreach ($query->getResult() as $row)
+		{
+		//Initialize User Info Array
+		$employeeList = array('employee_id' => $row->employee_ID, 'firstname' => $row->firstname, 'surname' => $row->surname, 'email' => $row->email, 'phone_no' => $row->phone_no);
+		}
+
+		//Return array
+		return $employeeList;	
+	}
+
+	public function deleteEmployee($employee_id)
+	{
+		//Query
+		if ($this->db->query("UPDATE employees SET is_deleted = 1 WHERE employee_id = '$employee_id'"))
+		{
+		    $confirmation = "Successful";
+		}
+		else
+		{
+		    $confirmation = "Unsuccessful";
+		}
+
+		//Return array
+		return $confirmation;
+	}
+
+	public function registerEmployee($firstName, $surname, $email, $phone_no, $password)
+	{
+		//Query
+		//1. Insert into employees table
+		if ($this->db->query("INSERT INTO employees (firstname, surname, email, phone, role_id, password) VALUES ('$firstName', '$surname', '$email', '$phone_no', "1", '$password')"))
+		{
+			//2. Retrieve employee ID of newly added employee
+	        $query = $this->db->query("SELECT employee_id FROM employees WHERE firstname ='$firstname' AND surname = '$surname'");
+
+	        //Store details
+	        foreach ($query->getResult() as $row)
+	        {
+	            //Initialize Array
+	            $newEmployee = array('employee_id' => $row->employee_id);
+	        }
+	        $employee_id = $newEmployee['employee_id']);
+
+			//3. Create a new row in the payslip table
+			if ($this->db->query("INSERT INTO payslip (employee_id) VALUES ('$employee_id')"))
+			{
+				$confirmation = "Successful";
+			}			
+		}
+		else
+		{
+		    $confirmation = "Unsuccessful";
+		}
+
+		//Return array
+		return $confirmation;
+
+		//TEST
+		//return "New user added: Name - $firstName $surname";
+	}
+
+	public function employeeFocus($employee_id)
+	{
+		//Temporarily define $user_info
+		$employeeDetails = array();
+
+		//Query
+		$query = $this->db->query("
+			SELECT employee_ID, firstname, surname, email, phone_no
+			FROM employees 
+			WHERE employee_id = '$employee_id'
+			");
+
+		//Store details in array
+		foreach ($query->getResult() as $row)
+		{
+		//Initialize User Info Array
+		$employeeDetails = array('employee_id' => $row->employee_ID, 'firstname' => $row->firstname, 'surname' => $row->surname, 'email' => $row->email, 'phone_no' => $row->phone_no);
+		}
+
+		//Return array
+		return $employeeDetails;	
+	}
 }
