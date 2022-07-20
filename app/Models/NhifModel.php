0ab593e75952cc80d7bca62205f175beb0491da5
mkaminchia <michaelkaminchia.mk@gmail.com>
2022-07-20 03:11:20 +0300

    NHIF calculation correction

--- app/Models/NhifModel.php
+++ app/Models/NhifModel.php
@@ -38,16 +38,7 @@ class NhifModel extends Model
 
     public function computeNhif($employee_id)
     {
-        //1. Initialize values for gross_salary, benefit, percentage, relief, relief_percentage, prev_cut_off and current_cut_off
-        $gross_salary = 0;
-        $benefit = 0;
-        $percentage = 0;
-        $relief = 0;
-        $prev_cut_off = 0;
-        $current_cut_off = 0;
-        $relief_percentage = 0;
-
-        //Retrieve gross_salary from payslip where employee_ID = employee_id
+        //1. Retrieve the gross_salary
         //Query
         $query = $this->db->query("
             SELECT gross_salary
@@ -58,10 +49,10 @@ class NhifModel extends Model
         //Store details in array
         foreach ($query->getResult() as $row)
         {
-            $gross_salary = intval($row->gross_salary);
+            $gross_salary = $row->gross_salary;
         }
 
-        //Retrieve relief_percentage from benefits table where benefit_name = nhif
+        //2. Retrieve relief_percentage from benefits table where benefit_name = nhif
         //Query
         $query = $this->db->query("
             SELECT relief_percentage
@@ -75,48 +66,24 @@ class NhifModel extends Model
             $relief_percentage = intval($row->relief_percentage);
         }
 
-        //2. Retrieve nhif bracket rows from nhif brackets table
+        //2. Identify the employee's bracket and retrieve the MPA
         //Query
         $query = $this->db->query("
-            SELECT *
-            FROM `nhif-brackets` 
-            WHERE is_deleted = 0
+            SELECT amount
+            FROM `pay-slip`
+            WHERE cut_off <= '$gross_salary'
+            ORDER BY cut_off DESC
+            LIMIT 1;
             ");
 
         //Store details in array
         foreach ($query->getResult() as $row)
         {
-            $current_cut_off = intval($row->cut_off);
-            $percentage = intval($row->percentage);
-
-            if( $gross_salary > $current_cut_off )
-            {
-                //benefit for the nhif bracket
-                $benefit = $benefit + (($current_cut_off - $prev_cut_off) * $percentage);
-                
-                //update prev_cut_off
-                $prev_cut_off = intval($row->current_cut_off);
-            }
-            elseif( $gross_salary <= $current_cut_off )
-            {
-                //benefit for the nhif bracket
-                $benefit = $benefit + (($gross_salary - $prev_cut_off) * $percentage);
-                
-                //update prev_cut_off
-                $prev_cut_off = intval($row->current_cut_off);
-            }
-
-        }
-
-        //Case: gross_salary is greater than the last cut_off amount
-        if($gross_salary > $current_cut_off)
-        {
-            //benefit for remaining amount
-            $benefit = $benefit + (($gross_salary - $current_cut_off) * $percentage);
+            $benefit = $row->amount;
         }
 
         //Calculate relief
-        $relief = $benefit * $relief_percentage;
+        $relief = ($benefit * ($relief_percentage/100));
 
         //Input $benefit and $relief into the employee-nhif-details table for that employee
         if($this->db->query("
